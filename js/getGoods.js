75c45cbd9c0679203eb81df51aec774b7b770792
OlBul <izotova_o@ukr.net>
2021-11-22 14:25:21 +0200

    Add files via upload

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,6 +1,8 @@
 const getGoods = () => {
 
     const links = document.querySelectorAll('.navigation-link')
+    const more = document.querySelector('.more');
+    const cartBtn = document.querySelector('.button-cart');
 
     const renderGoods = (goods) => {
         const goodsContainer = document.querySelector('.long-goods-list');
@@ -56,6 +58,13 @@ const getGoods = () => {
 
     if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
         renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
+
+    if (more) {
+        more.addEventListener('click', () => {
+            event.preventDefault();
+            getData();
+        })
     }
 }
 
--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 const search = function () {
     const input = document.querySelector('.search-block > input');
     const searchBtn = document.querySelector('.search-block > button');
+    const cartBtn = document.querySelector('.button-cart');
 
 
     const renderGoods = (goods) => {
